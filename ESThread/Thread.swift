e66f31bb4326bb11e7261a083aeb3212278ead13
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-08-01 19:53:31 +0900

    Add `makeQueue` function.

--- ESThread/Thread.swift
+++ ESThread/Thread.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+public func makeQueue(identifier:String) -> dispatch_queue_t {
+	
+	return makeQueue(identifier, attribute: nil)
+}
+
+public func makeQueue(identifier:String, attribute:dispatch_queue_attr_t?) -> dispatch_queue_t {
+	
+	return dispatch_queue_create(identifier, attribute)
+}
+
 public func sleepForSecond(second:Double) {
 	
 	NSThread.sleepForTimeInterval(second)
